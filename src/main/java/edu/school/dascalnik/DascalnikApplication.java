9b609865cdce2e648b19f2d985f10c229baecde5
anast <anast@DESKTOP-8RB94TT>
2022-03-09 13:02:16 +0200

    Commit spring

---
+++ src/main/java/edu/school/dascalnik/DascalnikApplication.java
@@ -0,0 +1,13 @@
+package edu.school.dascalnik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DascalnikApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DascalnikApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/school/dascalnik/DascalnikApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.school.dascalnik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DascalnikApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
